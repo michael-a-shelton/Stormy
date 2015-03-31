fa6b97bb567206958b34bb8fa9a590ec25a689ff
woohoo1us <michael.a.shelton@hotmail.com>
2015-03-31 18:40:18 -0400

    Airport

--- app/src/main/java/com/example/michael/stormy/ui/DailyForecastAcitivity.java
+++ app/src/main/java/com/example/michael/stormy/ui/DailyForecastAcitivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 
 import com.example.michael.stormy.R;
 
@@ -16,6 +17,11 @@ public class DailyForecastAcitivity extends ListActivity {
         setContentView(R.layout.activity_daily_forecast_acitivity);
         String[] daysOfTheWeek = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
 
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, daysOfTheWeek);
+
+        setListAdapter(adapter);
+        
 
     }
 
